253777ae4781c19991008d5e308826278b560d8d
/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/stevebrownlee/b146bf49071c46c41eddf5778b147a71/raw/a1f8f3bebf7fab75e5bfb0d1ecabc89ab22baec8/verify-installs.sh) <christinam1215@yahoo.com>
2024-08-03 14:47:50 -0500

    Added string representation for each class

--- slithering/RatSnake.py
+++ slithering/RatSnake.py
@@ -10,5 +10,8 @@ class RatSnake:
         self.slithering = True
         self.food = food
 
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
